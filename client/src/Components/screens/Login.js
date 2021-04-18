a76e4af6ec0d9bdbc519424643af08e216bb72b8
garushi <garushiarora1999@gmail.com>
2021-04-18 21:25:37 +0530

    signup screen

--- client/src/Components/screens/Login.js
+++ client/src/Components/screens/Login.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import '../../App.css';
 const Login=()=>{
-    return <h1>Login</h1>
+    return (
+        <div className="top-card">
+            <div className="auth-card">
+                <h2>Sharing</h2>
+                <input className="signin_input" type="text"
+                placeholder="email"
+                />
+                <input className="signin_input" type="text"
+                placeholder="password"
+                />
+                <button className="signin_button">
+                    SignIn
+                </button>
+            </div>
+        </div>
+    )
 }
 export default Login;
