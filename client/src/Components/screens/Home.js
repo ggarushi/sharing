2fc3795b0ae720a63c46f5012f6c0a2c851eb885
garushi <garushiarora1999@gmail.com>
2021-04-19 13:00:01 +0530

    changes in home screen

--- client/src/Components/screens/Home.js
+++ client/src/Components/screens/Home.js
@@ -1,5 +1,19 @@
 import React from 'react';
 const Home=()=>{
-    return <h1>hii</h1>
+    return (
+        <div className="Outer-card">
+            <div className="inner-card">
+                <h5>ramesh</h5>
+                <div className="card-image">
+                <img className="item" src="https://images.unsplash.com/photo-1618678419688-a6dd93d0aa10?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="profile"/>
+                </div>
+                <div className="card-content">
+                    <h6>title</h6>
+                    <p>amazing post</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+        </div>
+    )
 }
 export default Home;
