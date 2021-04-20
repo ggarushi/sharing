0ee8a1e9e02970a684682f5a0c020bff52214ea3
garushi <garushiarora1999@gmail.com>
2021-04-20 21:11:23 +0530

    signup connected

--- client/src/Components/screens/Signup.js
+++ client/src/Components/screens/Signup.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Link} from 'react-router-dom';
 const Signup=()=>{
+    const [name,setName]=useState("");
+    const [password,setPassword]=useState("");
+    const [email,setEmail]=useState("");
+    const PostData=()=>{
+        fetch("/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name:"",
+                password:"",
+                email:""
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+    }
     return (
         <div className="top-card">
         <div className="auth-card">
             <h2>Sharing</h2>
             <input className="signin_input" type="text"
             placeholder="name"
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
             />
             <input className="signin_input" type="text"
             placeholder="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             />
             <input className="signin_input" type="text"
             placeholder="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
             />
-            <button className="signin_button">
+            <button className="signin_button" onClick={()=>PostData()}>
                 SignUP
             </button>
             <h5 className="bottom">
