c40a3a3be2f55f9785b42ba2442905ec248c5053
garushi <garushiarora1999@gmail.com>
2021-04-20 22:27:22 +0530

    signup set

--- client/src/Components/screens/Signup.js
+++ client/src/Components/screens/Signup.js
@@ -1,23 +1,38 @@
 import React,{useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link,useHistory} from 'react-router-dom';
+import M from 'materialize-css';
+import '../../App.css';
 const Signup=()=>{
     const [name,setName]=useState("");
     const [password,setPassword]=useState("");
     const [email,setEmail]=useState("");
+    const history=useHistory();
     const PostData=()=>{
+        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+            M.toast({html: "invalid email",classes:"#c62828 red darken-3"})
+            return
+        }
         fetch("/signup",{
             method:"post",
             headers:{
                 "Content-Type":"application/json"
             },
             body:JSON.stringify({
-                name:"",
-                password:"",
-                email:""
+                name,
+                password,
+                email
             })
         }).then(res=>res.json())
         .then(data=>{
-            console.log(data)
+           if(data.error){
+               M.toast({html:data.error,classes:"#3f51b5 indigo"})
+           }
+           else{
+               M.toast({html:data.message,classes:"#43a047 green darken-1"})
+               history.push('/signin');
+           }
+        }).catch(error=>{
+            console.log(error);
         })
     }
     return (
