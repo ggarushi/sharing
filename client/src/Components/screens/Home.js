1b1b8328d00538d50d1bc2ccc8a3ffc877ff983f
garushi <garushiarora1999@gmail.com>
2021-04-21 20:08:39 +0530

    like unlike

--- client/src/Components/screens/Home.js
+++ client/src/Components/screens/Home.js
@@ -22,6 +22,56 @@ const Home=()=>{
             setData(result.posts)
         })
     },[])
+    const likePost = (id)=>{
+        fetch('/like',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+                 //   console.log(result)
+          const newData = data.map(item=>{
+              if(item._id==result._id){
+                  return result
+              }else{
+                  return item
+              }
+          })
+          setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+  }
+  const unlikePost = (id)=>{
+        fetch('/unlike',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId:id
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+          //   console.log(result)
+          const newData = data.map(item=>{
+              if(item._id==result._id){
+                  return result
+              }else{
+                  return item
+              }
+          })
+          setData(newData)
+        }).catch(err=>{
+          console.log(err)
+      })
+  }
     return (
         <div className="Outer-card">
            {
@@ -33,7 +83,10 @@ const Home=()=>{
                 <img className="ca-img" src={item.photo} alt="profile"/>
                 </div>
                 <div className="card-content">
-                <i className="fa fa-thumbs-up" aria-hidden="true"></i>
+                {item.likes.includes(state._id)
+                ? <i className="fa fa-thumbs-down" style={{marginLeft:"10px"}} aria-hidden="true" onClick={()=>{unlikePost(item._id)}}></i>
+                : <i className="fa fa-thumbs-up" aria-hidden="true" onClick={()=>{likePost(item._id)}}></i>}
+                    <h6>{item.likes.length} likes</h6>
                     <h6>{item.title}</h6>
                     <p style={{fontSize:"1.3em"}}>{item.body}</p>
                     <input className="card-input" type="text" placeholder="add a comment"/>
